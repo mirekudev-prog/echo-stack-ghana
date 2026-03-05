a8d661bdaf9bf6f7269c10446d9ce434e5630f21
mirekudev-prog <mireku.dev@gmail.com>
2026-03-05 00:45:40 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Uses SQLite file in same folder — NO EXTERNAL CONNECTION
+SQLALCHEMY_DATABASE_URL = "sqlite:///./echo_stack.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
