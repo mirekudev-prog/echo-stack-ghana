e79142490f50ac13c90c6f08c598f71c19a54d8c
mirekudev-prog <mireku.dev@gmail.com>
2026-03-05 14:10:07 +0000

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# SQLite works on Render free tier
 SQLALCHEMY_DATABASE_URL = "sqlite:///./echostack.db"
 
 engine = create_engine(
